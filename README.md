6ecdf3fb6e0a6624b417f484aad7875bfe79921d
Satyakoidala <koidalasai21@gmail.com>
2022-12-18 17:52:24 +0530

    feat(): initial linting, formatting pkgs are added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # create-react-app-demo-without-CRA
+
 Setup demo for creating react application without using CRA or any other framework.
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,73 @@
-const path = require('path');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	mode: "development",
-	entry: "./index.js",
+	entry: {
+		bundle: path.resolve(__dirname, "index.js"),
+	},
 	output: {
-		path: path.resolve(__dirname, "public"),
-		filename: "[name]_debug.js",
+		path: path.resolve(__dirname, "dist"),
+		filename: "debug_[name]_[contenthash].js",
+		clean: true,
+		assetModuleFilename: "[name][ext]",
 	},
 	target: "web",
 	devServer: {
 		port: "9009",
-		static: ["./public"],
+		static: {
+			directory: path.resolve(__dirname, "dist"),
+		},
 		open: true,
 		hot: true,
-		liveReload: true
+		compress: true,
+		historyApiFallback: true,
+		liveReload: true,
 	},
 	resolve: {
-		extensions: ['.js', '.jsx', '.json']
+		extensions: [".js", ".jsx", ".json"],
 	},
 	module: {
 		rules: [
 			{
-				test: /\.(js|jsx)$/,    //kind of file extension this rule should look for and apply in test
-				exclude: /node_modules/, //folder to be excluded
-				use: 'babel-loader' //loader which we are going to use
-			}
-		]
-	}
-}
+				test: /\.(js|jsx)$/, // kind of file extension this rule should look for and apply in test
+				exclude: /node_modules/, // folder to be excluded
+				use: "babel-loader", // loader which we are going to use
+			},
+			{
+				test: /\.scss$/i,
+				exclude: /node_modules/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "debug_[name]_[contenthash].css",
+		}),
+		new HTMLWebpackPlugin({
+			title: "React App",
+			filename: "index.html",
+			template: "src/pages/homepage.html",
+		}),
+	],
+};
